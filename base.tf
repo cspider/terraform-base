b04a5befaa4fa26b22797c709b8ad043795e43e5
cspider <itssusanta@gmail.com>
2020-03-01 23:07:43 -0500

    All variables are in terraform.tfvars file

--- base.tf
+++ base.tf
@@ -14,12 +14,14 @@ provider "aws" {
 resource "aws_instance" "base" {
   //ami           = var.ami[var.region]
   // or
-  ami           = lookup(var.ami, var.region)
-  instance_type = var.instance_type
-  key_name      = var.ami_key_pair_name
+  ami                    = lookup(var.ami, var.region)
+  instance_type          = var.instance_type
+  key_name               = var.ami_key_pair_name
+  vpc_security_group_ids = var.security_group_ids
 }
 
 resource "aws_eip" "base" {
   instance = aws_instance.base.id
   vpc      = true
 }
+
--- variables.tf
+++ variables.tf
@@ -1,33 +1,28 @@
 variable "region" {
   type        = string
   description = "The AWS Region"
-  default     = "us-east-1"
 }
 
 
-variable "ami_key_pair_name" {
-
-}
+variable "ami_key_pair_name" {}
 
 variable "region_list" {
-
   type        = list(string)
   description = "AWS Availability zones"
-  default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "ami" {
   type        = map
   description = "Map of region vs ami id"
-  default = {
-    us-east-1 = "ami-0d729a60"
-    us-west-1 = "ami-7c4b331c"
-  }
 }
 
 variable "instance_type" {
   type        = string
   description = "AWS EC2 AMI Instance Type"
-  default     = "t2.micro"
 }
 
+variable "security_group_ids" {
+  type        = list(string)
+  description = "List of security group ids"
+  default     = ["sg-bcb12ac5", "sg-0bbdd299a18471ed9"]
+}
